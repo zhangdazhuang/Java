a34cd573c255a62fbe3561394f4f2fec4ba335f0
ZJPHP <3283531191@qq.com>
2021-04-19 10:49:20 +0800

    1

---
+++ SortStrategy/SortFactory.java
@@ -0,0 +1,12 @@
+package SortStrategy;
+
+public class SortFactory {
+    public SortFactory(){}
+
+    public static SortHandle getInterface(String classname){
+        SortHandle sortHandle = null;
+
+        return sortHandle;
+    }
+
+}
--- Test/FileTest0/testjava.java
+++
@@ -1,5 +0,0 @@
-package Test.FileTest0;
-
-public class testjava {
-    
-}
